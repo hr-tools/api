54b48bdf5288029b0e44c36f3cfe4bea727acbda
shay <bakkcat@gmail.com>
2021-11-07 17:23:41 +0100

    Add note about ± sign in predicted color names

--- static/vision/page.js
+++ static/vision/page.js
@@ -13,7 +13,7 @@ const disclaimersModal = e(Modal, {
     children: [
         e('p', {className: 'disclaimer'}, e('a', {href: '/vision/breeds'}, 'Click here for a list of breeds that Realvision supports.')),
         e('br'),
-        e('p', {className: 'disclaimer'}, 'Base color selections are for eliminating inaccurate color predictions caused by duplicate image codes. We suggest using these fields if your horse is heavily diluted.'),
+        e('p', {className: 'disclaimer'}, 'Base color selections are for eliminating inaccurate color predictions caused by duplicate image codes. We suggest using these fields if your horse is heavily diluted. If your foal is predicted with a ± (plus-minus sign) in its color name and genotype, it means that this color shares image codes with a different genotype, and could be used for a horse with or without a certain gene. E.g., Smoky Cream ± Silver = your horse will look exactly like a smoky cream with or without silver.'),
         e('br'),
         e('p', {className: 'disclaimer'}, 'Realvision can predict rabicano and roan on untested foals by referencing white pattern codes that are changed due to the presence of the gene. However, these genes cannot be seen on foals.'),
         e('br'),
